ef681c781a4b3505409ca2c5dea4f59be90e5605
Maxime <maxime.antoine2@gmail.com>
2017-03-10 10:25:49 +0800

    FAKE implementation

--- src/Loggy.Web/src/api/userApi.js
+++ src/Loggy.Web/src/api/userApi.js
@@ -1,16 +1,34 @@
 'use strict';
 
-var UserAPI = {
-    getUsers: function(){
-        //TODO
-        return [
+var _ = require('lodash');
+
+//FAKE implementation so far
+var _users = [
             {id: 0, name: 'test 1'},
             {id: 1, name: 'test 2'},
             {id: 2, name: 'test3'}
-        ];
+];
+
+var UserAPI = {
+    getUsers: function(){
+        return _users;
     },
     getUserById: function(id){
-        //TODO
+        return _users.find(_users, {id: id});
+    },
+    deleteUser: function(id){
+        return _.remove(_users, function(user){
+            return user.id === id;
+        });
+    },
+    createUser: function(createdUser){
+        _users.push(createdUser);
+        return createdUser;
+    },
+    updateUser: function(updatedUser){
+        _user.remove(_users, {id: updatedUser.id});
+        _users.push(updatedUser);
+        return updatedUser;
     }
 };
 
